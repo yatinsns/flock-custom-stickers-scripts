326426c544c391afeb4ad32d39d5b713b040ba0f
Yatin Sarbalia <yatinsns@gmail.com>
2015-05-30 13:03:58 +0530

    Fix sticker_parser script to download all stickers

--- lib/sticker_parser.rb
+++ lib/sticker_parser.rb
@@ -1,34 +1,54 @@
 #! /usr/bin/env ruby
-
 require 'json'
 
-META_DATA_URL = "https://mc.flock.co"
+def fetch_sticker_meta_data(url)
+  meta_data = `curl #{url}`
+  JSON.parse meta_data
+end
 
-stickers_meta_data_result = `curl #{META_DATA_URL}`
-stickers_meta_data = JSON.parse(stickers_meta_data_result)
-collections = stickers_meta_data["collections"]
-sets = collections[0]["sets"]
+def parsed_info_from_meta_data(meta_data)
+  collections = meta_data["collections"]
+  sets = collections[0]["sets"]
 
-results = {}
-sets.each do |set|
-  sticker_set_name = set["id"]
-  set["items"].each do |item|
-    sticker_name = item["name"].downcase
-    sticker_id = item["id"]
-    sticker_source = item["source"]
-    if results[sticker_name].nil?
-      results[sticker_name] = []
+  results = {}
+  sets.each do |set|
+    sticker_set_name = set["id"]
+    set["items"].each do |item|
+      sticker_name = encode item["name"]
+      sticker_source = item["source"]
+      update_results(results, sticker_name, sticker_source)
     end
-    results[sticker_name].push sticker_source
+  end
+  results
+end
+
+def encode(string)
+  string.downcase.gsub("(", "").gsub(")", "").gsub("'", "").gsub("?", "")
+end
+
+def update_results(results, key, value)
+  if results[key].nil?
+    results[key] = []
+  end
+  results[key].push value
+end
+
+def download_stickers_from_parsed_info(parsed_info)
+  parsed_info.each do |key, value|
+    puts "#{key} (#{value.count})"
+    dir_name = key.gsub(' ', '-')
+    `mkdir ../stickers/#{dir_name}`
+    index = 0
+    value.each do |url|
+      `curl #{url} > ../stickers/#{dir_name}/#{index}.png`
+      index = index + 1
+    end
   end
 end
 
-results.each do |key, value|
-  puts "#{key} (#{value.count})"
+def main
+  parsed_info = parsed_info_from_meta_data(fetch_sticker_meta_data ARGV[0])
+  download_stickers_from_parsed_info parsed_info
 end
 
-
-
-
-
-
+main if __FILE__ == $0
