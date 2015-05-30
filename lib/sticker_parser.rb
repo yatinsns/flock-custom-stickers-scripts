fb921e3a2e31bab51a5434e6445bde2bc5d4a396
Yatin Sarbalia <yatinsns@gmail.com>
2015-05-30 11:46:31 +0530

    Parse stickers meta data to create name-urls info

--- lib/sticker_parser.rb
+++ lib/sticker_parser.rb
@@ -1,7 +1,34 @@
 #! /usr/bin/env ruby
 
+require 'json'
+
 META_DATA_URL = "https://mc.flock.co"
 
-stickers_meta_data = `curl #{META_DATA_URL}`
-puts "Stickers metadata : #{stickers_meta_data}"
+stickers_meta_data_result = `curl #{META_DATA_URL}`
+stickers_meta_data = JSON.parse(stickers_meta_data_result)
+collections = stickers_meta_data["collections"]
+sets = collections[0]["sets"]
+
+results = {}
+sets.each do |set|
+  sticker_set_name = set["id"]
+  set["items"].each do |item|
+    sticker_name = item["name"].downcase
+    sticker_id = item["id"]
+    sticker_source = item["source"]
+    if results[sticker_name].nil?
+      results[sticker_name] = []
+    end
+    results[sticker_name].push sticker_source
+  end
+end
+
+results.each do |key, value|
+  puts "#{key} (#{value.count})"
+end
+
+
+
+
+
 
